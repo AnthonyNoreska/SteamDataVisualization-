19ba95d529daa2a097e225135afd32f76a06d69a
jmoodina <josh_modz@hotmail.com>
2023-12-05 18:36:38 -0500

    Flask Update

--- app.py
+++ app.py
@@ -23,6 +23,41 @@ def genre_totals():
     df3 = pd.read_csv("genre_totals.csv")
     return jsonify(df3.to_dict())
 
+@app.route("/api/adventure")
+def adventure_api():
+    adventure_output = pd.read_csv("Adventure_data_converted.csv")
+    return jsonify(adventure_output.to_dict())
+
+@app.route("/api/action")
+def action_api():
+    action_output = pd.read_csv("Action_data_converted.csv")
+    return jsonify(action_output.to_dict())
+
+@app.route("/api/indie")
+def indie_api():
+    indie_output = pd.read_csv("Indie_data_converted.csv")
+    return jsonify(indie_output.to_dict())
+
+@app.route("/api/RPG")
+def RPG():
+    rpg_output = pd.read_csv("RPG_data_converted.csv")
+    return jsonify(rpg_output.to_dict())
+
+@app.route("/api/simulation")
+def simulation():
+    simulation_output = pd.read_csv("Simulation_data_converted.csv")
+    return jsonify(simulation_output.to_dict())
+
+@app.route("/api/sports")
+def sports():
+    sports_output = pd.read_csv("Sports_data_converted.csv")
+    return jsonify(sports_output.to_dict())
+
+@app.route("/api/strategy")
+def strategy():
+    strategy_output = pd.read_csv("Strategy_data_converted.csv")
+    return jsonify(strategy_output.to_dict())
+
 
 if __name__ == "__main__":
     app.run()
